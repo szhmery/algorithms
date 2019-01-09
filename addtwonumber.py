4a112634be271798529d747aa6fc1eac5a88b365
szhmery <szhmery@gmail.com>
2019-01-09 17:32:59 +0800

    modify addtownumber code

--- addtwonumber.py
+++ addtwonumber.py
@@ -11,15 +11,22 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        # 通过生成器把l1的数据都取出,转成str,存入list1中
         list1 = [str(i) for i in my_generator(l1)]
         print(list1)
+        # 通过生成器把l2的数据都取出,转成str,存入list2中
         list2 = [str(i) for i in my_generator(l2)]
         print(list2)
+        # 把list1中的所有值连到一起并反向,之后转为int
         print(int("".join(list1[::-1])))
+        # 把list2中的所有值连到一起并反向,之后转为int
         print(int("".join(list2[::-1])))
-        print(map(int, str(int("".join(list1[::-1]))  + int("".join(list2[::-1])))[::-1]))
+        # 两个数相加后又转回str,然后map成int,list把map中数又转为list
         return list(map(int, str(int("".join(list1[::-1]))  + int("".join(list2[::-1])))[::-1]))
+        #return list(str(int("".join(list1[::-1])) + int("".join(list2[::-1])))[::-1])
 
+
+#生成器,返回linked list中的数据并返回。
 def my_generator(linked_list):
     generator_list = linked_list
     while generator_list:
@@ -29,10 +36,10 @@ def my_generator(linked_list):
 # l1=[2,4,3]
 # l2=[5,6,4]
 
-l1 = ListNode(2)
+l1 = ListNode(2)  #节点2 做头
 l1_2 = ListNode(4)
-l1.next = l1_2
-l1_2.next = ListNode(3)
+l1.next = l1_2  #节点4,放在2的后面
+l1_2.next = ListNode(3) #节点3,放在4的后面
 l2 = ListNode(5)
 l2_2 = ListNode(6)
 l2.next = l2_2
