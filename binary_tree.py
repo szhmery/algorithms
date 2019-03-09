36f8cdf1127a6b319c11cd639b611a6b81cfb2aa
szhmery <szhmery@gmail.com>
2019-03-09 17:15:40 +0800

    update binary_tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,4 +1,6 @@
 #coding=utf-8
+import queue
+
 
 class Node(object):
     """节点类"""
@@ -122,6 +124,97 @@ class Tree(object):
             if node.rchild != None:
                 myQueue.append(node.rchild)
 
+    def levelOrder(self,root):
+        """BFS层次"""
+        res = []
+        if root == None:
+            return res
+
+        q = [root]
+        while len(q) != 0:
+            res.append([node.elem for node in q])
+            new_q = []
+            for node in q:
+                if node.lchild:
+                    new_q.append(node.lchild)
+                if node.rchild:
+                    new_q.append(node.rchild)
+            q = new_q
+
+        return res
+
+
+
+    def treeDepth(self,tree):
+        if tree == None:
+            return 0
+        leftDepth = self.treeDepth(tree.lchild)
+        rightDepth = self.treeDepth(tree.rchild)
+        if leftDepth > rightDepth:
+            return leftDepth + 1
+        if rightDepth >= leftDepth:
+            return rightDepth + 1
+
+    def button_up_maxDepth(self, root):
+        if root == None:
+            return 0
+        leftmax = self.button_up_maxDepth(root.lchild)
+        rightmax = self.button_up_maxDepth(root.rchild)
+        return max(leftmax, rightmax) + 1
+
+    answer = 0
+    def top_down_maxDepth(self, root,deepth):
+        if root == None:
+            return
+        if root.lchild == None and root.rchild == None:
+            self.answer= max(self.answer,deepth)
+            #print(self.answer)
+
+        self.top_down_maxDepth(root.lchild,deepth+1)
+        self.top_down_maxDepth(root.rchild,deepth+1)
+
+
+    def treeWidth(self,tree):
+        curwidth = 1
+        maxwidth = 0
+        q = queue.Queue()
+        q.put(tree)
+        while not q.empty():
+            n = curwidth
+            for i in range(n):
+                tmp = q.get()
+                curwidth -= 1
+                if tmp.rchild:
+                    q.put(tmp.lchild)
+                    curwidth += 1
+                if tmp.rchild:
+                    q.put(tmp.rchild)
+                    curwidth += 1
+            if curwidth > maxwidth:
+                maxwidth = curwidth
+        return maxwidth
+
+    def help(self, p, q):
+        if p == None and q == None:
+            return True
+        if p and q and p.elem == q.elem:
+            return self.help(p.rchild, q.lchild) and self.help(p.lchild, q.rchild)#递归判断左子树根的右子树根节点和右子树根的左子树根节点以及左子树根的左子树根节点和右子树根的右子树根节点的值是否相等
+        return False
+
+    def isSymmetric(self, root):
+        if root:
+            return self.help(root.lchild, root.rchild)
+        return True
+
+    def hasPathSum(self, root, sum: int):
+        if root == None:
+            return False
+        if root.lchild == None and root.rchild == None:
+            return root.elem == sum
+        return self.hasPathSum(root.lchild,sum - root.elem) or self.hasPathSum(root.rchild, sum - root.elem)
+
+
+
 
 if __name__ == '__main__':
     """主函数"""
@@ -146,3 +239,28 @@ if __name__ == '__main__':
     tree.middle_stack(tree.root)
     print('\n堆栈实现后序遍历:')
     tree.later_stack(tree.root)
+    print('\n堆栈深度:')
+    print(tree.treeDepth(tree.root))
+    print('\n堆栈宽度:')
+    print(tree.treeWidth(tree.root))
+    print('\n堆栈分层排序:')
+    print(tree.levelOrder(tree.root))
+    print('\nbutton up堆栈深度:')
+    print(tree.button_up_maxDepth(tree.root))
+
+    print('\ntop down堆栈深度:')
+    tree.top_down_maxDepth(tree.root,1)
+    print(tree.answer)
+
+    print('\n是否有path sum等于11:')
+    print(tree.hasPathSum(tree.root,8))
+
+
+    elems2 = [1, 2, 2, 3, 4, 4, 3]
+    tree2 = Tree()  # 新建一个树对象
+    for elem in elems2:
+        tree2.add(elem)  # 逐个添加树的节点
+    print('\n堆栈分层排序:')
+    print(tree2.levelOrder(tree2.root))
+    print('\n是否是堆成二叉树:')
+    print(tree2.isSymmetric(tree2.root))
