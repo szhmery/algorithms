defbd4339eda5968ff8c2846eb43e82cac23210b
szhmery <szhmery@gmail.com>
2019-06-04 18:47:42 +0800

    modify longestPabindrome

--- leetcode/longestPalindrome.py
+++ leetcode/longestPalindrome.py
@@ -3,28 +3,56 @@
 
 class Solution():
     def longestPalindrome(self,s):
+        """
+            :type s: str
+            :rtype: str
+            """
         longestLen = 0
-        longestSub = []
-        for i in range(len(s)-1):
-            for j in range(i+1,len(s)):
-
-                if Solution._isPalindrome(self,s[i:j]) == True:
+        longestSub = ""
+        if len(s) > 1000:
+            return False
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                if Solution._isPalindrome(self,s[i:j]) == True and len(s[i:j]) > len(longestSub):
                     print(s[i:j])
-                    if len(s[i:j]) > longestLen:
-                        longestSub = s[i:j]
-                        longestLen = len(s[i:j])
+                    longestSub = s[i:j]
+
         print("LongestPalindrome: ", longestSub)
         return longestSub
 
     def _isPalindrome(self,s):
         if s == None:
             return False
-
-        if  s == s[::-1]:
+        if s == s[::-1]:
             return True
         else:
             return False
 
+    def longestPalindrome(self,s):
+
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Return if string is empty
+        if not s: return s
+
+        res = ""
+        for i in range(len(s)):
+            j = i + 1
+            # While j is less than length of string
+            # AND res is *not* longer than substring s[i:]
+            while j <= len(s) and len(res) <= len(s[i:]):
+                # If substring s[i:j] is a palindrome
+                # AND substring is longer than res
+                if s[i:j] == s[i:j][::-1] and len(s[i:j]) > len(res):
+                    res = s[i:j]
+                j += 1
+
+        return res
+
 s = Solution()
 print(s.longestPalindrome("babad"))
 print(s.longestPalindrome("cbbd"))
+print(s.longestPalindrome(""))
+print(s.longestPalindrome("a"))
