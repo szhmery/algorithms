8c179ff919317d9ea00d356e10a2cd2baabb4c98
szhmery <szhmery@gmail.com>
2019-06-19 23:12:15 +0800

    add removeNthFromEnd

--- leetcode/removeNthFromEnd.py
+++ leetcode/removeNthFromEnd.py
@@ -6,7 +6,7 @@ class ListNode:
         self.val = x
         self.next = None
 
-
+    #打印内置函数,用print就可以把链表打印出来
     def __repr__(self):
         if self is None:
             return "Nil"
@@ -17,6 +17,8 @@ class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         dummp = ListNode(-1)
         dummp.next = head
+        #用快慢两个链表。先滑动快的n位。然后同时再一起滑动快慢,直到fast结束。这时候slow就处于倒数n的位置。
+        #用slow.next = slow.next.next删除倒数n的node
         slow, fast = dummp, dummp
 
         for i in range(n):
